6f9b2ea3ba7278a0e8e12ae09f4fb3fcca00b360
MorenoDaniela <47337445+MorenoDaniela@users.noreply.github.com>
2019-08-21 22:05:08 -0300

    Add files via upload

---
+++ Ejercicio 3 Moreno/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3_Moreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 3" ;
+
+            Console.WriteLine("Ingrese un numero: ");
+            string aux = Console.ReadLine();
+            int numero;
+
+            if (int.TryParse(aux,out numero))
+            {
+                int i=0;
+
+                for (int j=2; j<=numero; j++)
+                {
+                    for (i=2; i<j; i++)
+                }
+                
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
