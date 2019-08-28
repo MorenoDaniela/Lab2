9646cabe39b2f1e0dd26813811d3415f6c4d44bb
MorenoDaniela <denu.moreno.1990@gmail.com>
2019-08-28 03:19:32 -0300

    faltan correcciones

---
+++ Ejercicios/Ejercicio 10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int espacios = 21;
+            int inicio = 2;
+
+            string cadena = "                    *********";
+            Console.WriteLine("", cadena);
+            for (i=1;i<6;i++)
+            {
+                string resultado = cadena.Substring(inicio+2, espacios+2);
+                resultado = resultado + "**";
+                Console.WriteLine("", resultado);
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio 9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese cantidad de pisos de la piramide: ");
+            string pisos = Console.ReadLine();
+            int cantPisos;
+            
+            if (int.TryParse(pisos, out cantPisos))
+            {
+                if (cantPisos>=1)
+                {
+                    int i;
+                    string aux="*";
+                    Console.WriteLine("{0}", aux);
+                    for (i=1;i<cantPisos;i++)
+                    {
+                        aux = "**" + aux;
+                        Console.WriteLine("{0}", aux);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
