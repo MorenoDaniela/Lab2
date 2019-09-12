b205431edb9033ceba0a4abf3e94701cd624d031
MorenoDaniela <denu.moreno.1990@gmail.com>
2019-09-12 15:13:29 -0300

    revisar conversion estoy harta

---
+++ Ej23/Ej23/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Moneda;
+
+namespace Ej23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            double numero;
+            double.TryParse(textBox1.Text, out numero);
+            
+            //Dolar d = new Dolar(numero);
+            Euro eu = new Euro(numero);
+
+            textBox7.Text = numero.ToString();
+            textBox8.Text = ((Dolar)eu).GetCantidad().ToString();
+            textBox9.Text = ((Peso)eu).GetCantidad().ToString();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = Euro.GetCotizacion().ToString();
+            textBox5.Text = Dolar.GetCotizacion().ToString();
+            textBox6.Text = Peso.GetCotizacion().ToString();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            double numero2;
+            double.TryParse(textBox2.Text, out numero2);
+
+            Euro euro = new Euro(numero2);
+            Peso peso = new Peso(numero2);
+            Dolar dolar = new Dolar(numero2);
+
+            textBox11.Text = numero2.ToString();
+            textBox10.Text = ((Euro)dolar).GetCantidad().ToString();
+            textBox12.Text = ((Dolar)peso).GetCantidad().ToString();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            double numero3;
+            double.TryParse(textBox3.Text, out numero3);
+
+            Euro euro = new Euro(numero3);
+            Dolar dolar = new Dolar(numero3);
+            Peso peso = new Peso(numero3);
+
+            textBox15.Text = numero3.ToString();
+            textBox13.Text = ((Euro)peso).GetCantidad().ToString();
+            textBox14.Text = ((Dolar)peso).GetCantidad().ToString();
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ej23/Moneda/Peso.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moneda
+{
+#pragma warning disable CS0660, CS0661//desacctivar warnings
+    public class Peso
+    {
+        private double cantidad;
+        private static double cotizRespectoDolar;
+
+        static Peso()
+        {
+            //cotizRespectoDolar = 2;///0.50
+            cotizRespectoDolar = 38.33;
+        }
+
+        public Peso(double cantidad)
+        {
+            this.cantidad = cantidad;
+        }
+
+        public Peso(double cantidad, double cotizacion)
+            : this(cantidad)
+        {
+            cotizRespectoDolar = cotizacion;
+        }
+
+        public double GetCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public static double GetCotizacion()
+        {
+            return cotizRespectoDolar;
+        }
+
+        public static implicit operator Peso(double d)
+        {
+            Peso nuevo = new Peso(d);
+            return nuevo;
+            //return d;
+        }
+
+        public static explicit operator Dolar(Peso p)
+        {
+            return new Dolar(p.GetCantidad() * Peso.GetCotizacion());
+            //return (Dolar)(p.GetCantidad() / Dolar.GetCotizacion());
+        }
+
+        public static explicit operator Euro(Peso p)
+        {
+            return new Euro(p.GetCantidad() * 0.0223);//
+            //return new Euro(p.GetCantidad() * 0.25);//
+            //return (Euro)(p.GetCantidad() / Euro.GetCotizacion());
+        }
+
+        public static bool operator ==(Peso p, Dolar d)
+        {
+            bool retorno = false;
+
+            if (d == p)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Peso p, Dolar d)
+        {
+            bool retorno = false;
+            if (d != p)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator ==(Peso p, Euro e)
+        {
+            bool retorno = false;
+            if ((Euro)p == e)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Peso p, Euro e)
+        {
+            bool retorno = false;
+            if (!(p == e))
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator ==(Peso p1, Peso p2)
+        {
+            bool retorno = false;
+            if (p1 == p2)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Peso p1, Peso p2)
+        {
+            bool retorno = false;
+            if (!(p1 == p2))
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static Peso operator +(Peso p, Dolar d)
+        {
+            return new Peso(p.cantidad + ((Peso)d).cantidad);
+        }
+
+        public static Peso operator -(Peso p, Dolar d)
+        {
+            return new Peso(p.cantidad - ((Peso)d).cantidad);
+        }
+
+        public static Peso operator +(Peso p, Euro e)
+        {
+            return new Peso(p.cantidad + ((Peso)e).cantidad);
+        }
+
+        public static Peso operator -(Peso p, Euro e)
+        {
+            return new Peso(p.cantidad - ((Peso)e).cantidad);
+        }
+
+    }
+}
