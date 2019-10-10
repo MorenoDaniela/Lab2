d234297a635c83bb00da1e09fb2fad4abce5b75f
MorenoDaniela <denu.moreno.1990@gmail.com>
2019-10-10 16:20:43 -0300

    Ejemplo singleton

---
+++ PruebaSingleton/ConsoleApp1/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Singleton
+    {
+        private static Singleton instancia;
+
+        private Singleton()
+        {
+
+        }
+        public static Singleton CreateInstance()
+        {
+            if (instancia == null)
+            {
+                Singleton.instancia = new Singleton();
+                return Singleton.instancia;
+            }
+            else
+                return instancia;
+        }
+        public Singleton Instancia
+        {
+            get
+            {
+                return CreateInstance();
+            }
+        }
+        
+    }
+}
+
